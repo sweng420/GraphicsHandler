346853e16325467de3f500a1fe15a92761009845
Lou-E <45070408+Lou-E@users.noreply.github.com>
2019-06-01 10:41:53 +0100

    Add files via upload

--- src/GraphicsDisplay.java
+++ src/GraphicsDisplay.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
@@ -11,58 +12,73 @@ import javafx.stage.Stage;
 
 public class GraphicsDisplay extends Application
 {
+	HBox root;
+	Scene scene;
+	
 	@Override
 	public void start(Stage primaryStage)
 	{
-		//drawLine(primaryStage, 0, 0, 50, 50, 2, new Color(0.5, 0.6, 0.4, 1));
-		//drawRect(primaryStage, 50, 50, 20, 30, 2, new Color(0.5, 0.6, 0.4, 1));
-		//drawCircle(primaryStage, 50, 50, 20, 2, new Color(0.5, 0.6, 0.4, 1));
-		drawEllipse(primaryStage, 50, 50, 20, 10, 2, new Color(0.5, 0.6, 0.4, 1));
+		createScene(primaryStage, 400, 400);
+		
+		Line line = drawLine(primaryStage, 0, 0, 50, 50, 2, new Color(0.1, 0.6, 0.1, 1));
+		Rectangle rect = drawRect(primaryStage, 50, 50, 20, 30, 2, new Color(0.5, 0.4, 0.6, 1));
+		Circle circle = drawCircle(primaryStage, 50, 50, 20, 2, new Color(0.8, 0.6, 0.2, 1));
+		Ellipse ellipse = drawEllipse(primaryStage, 50, 50, 30, 15, 2, new Color(0.1, 0.2, 0.44, 1));
+		
+		addShape(line);
+		addShape(rect);
+		addShape(circle);
+		addShape(ellipse);
 	}
 	
-	public void drawLine(Stage stage, double startX, double startY, double endX, double endY, double width, Color colour)
+	public Line drawLine(Stage stage, double startX, double startY, double endX, double endY, double width, Color colour)
 	{
 		Line line = new Line(startX, startY, endX, endY);
 		line.setStrokeWidth(width);
 		line.setStroke(colour);
 		
-		createScene(stage, line);
+		return line;
 	}
 	
-	public void drawRect(Stage stage, double X, double Y, double width, double height, double thiccness, Color colour)
+	public Rectangle drawRect(Stage stage, double x, double y, double width, double height, double thiccness, Color colour)
 	{
-		Rectangle rect = new Rectangle(X, Y, width, height);
+		Rectangle rect = new Rectangle(x, y, width, height);
 		rect.setStrokeWidth(thiccness);
 		rect.setFill(colour);
 		
-		createScene(stage, rect);
+		return rect;
 	}
 	
-	public void drawCircle(Stage stage, double centreX, double centreY, double radius, double thiccness, Color colour)
+	public Circle drawCircle(Stage stage, double centreX, double centreY, double radius, double thiccness, Color colour)
 	{
 		Circle circle = new Circle(centreX, centreY, radius, colour);
 		circle.setStrokeWidth(thiccness);
 		
-		createScene(stage, circle);
+		return circle;
 	}
 	
-	public void drawEllipse(Stage stage, double centreX, double centreY, double radiusX, double radiusY, double thiccness, Color colour)
+	public Ellipse drawEllipse(Stage stage, double centreX, double centreY, double radiusX, double radiusY, double thiccness, Color colour)
 	{
 		Ellipse ellipse = new Ellipse(centreX, centreY, radiusX, radiusY);
 		ellipse.setStrokeWidth(thiccness);
 		ellipse.setFill(colour);
 		
-		createScene(stage, ellipse);
+		return ellipse;
 	}
 	
-	public void createScene(Stage stage, Shape shape)
+	public void createScene(Stage stage, int x, int y)
 	{
-		HBox root = new HBox();
-        root.getChildren().add(shape);
+		root = new HBox();
+		root.setAlignment(Pos.CENTER);
         
-        Scene scene = new Scene(root, 200, 100);
+        scene = new Scene(root, x, y);
         stage.setScene(scene);
         stage.show();
+	}
+	
+	public void addShape(Shape shape)
+	{
+        root.getChildren().add(shape);
 	}
 	
 	public static void main(String[] args)
